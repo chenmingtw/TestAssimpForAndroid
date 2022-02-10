6b7101a65fd26b64d10bd0c817019a3f50a63178
chenming_fan <chenming4tw@gmail.com>
2022-02-10 16:58:28 +0800

    Import Assimp and relative modules.

--- app/src/main/java/com/example/testassimpforandroid/MainActivity.kt
+++ app/src/main/java/com/example/testassimpforandroid/MainActivity.kt
@@ -3,20 +3,20 @@ package com.example.testassimpforandroid
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
-import com.example.testassimpforandroid.databinding.ActivityMainBinding
+//import com.example.testassimpforandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+//    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+//        binding = ActivityMainBinding.inflate(layoutInflater)
+//        setContentView(binding.root)
 
         // Example of a call to a native method
-        binding.sampleText.text = stringFromJNI()
+//        binding.sampleText.text = stringFromJNI()
     }
 
     /**
